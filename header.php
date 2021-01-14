839ba741a9b6697478993b555b1fc33b62f07083
TMB <tmb@TMB-1.local>
2021-01-14 14:59:11 +0100

    more responsive stuff

--- header.php
+++ header.php
@@ -31,6 +31,9 @@
 				<img src="<?php echo get_template_directory_uri();?>/content/images/logo.svg" alt="Logo" />
 			</div>
 			<div class="header__nav">
+				<div class="header__nav--hamburger">
+					<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="bars" class="svg-inline--fa fa-bars fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z"></path></svg>
+				</div>
 				<ul>
 					<li><a href="#">Expertises</a></li>
 					<li><a href="#">Projecten</a></li>
