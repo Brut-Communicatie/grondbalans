216dee662d1afe7abfdbe69dc28dd4bb015332e4
Leroy Davidson Descelles <leroy.davidsondescelles@zoolt.com>
2021-01-19 16:31:49 +0100

    vacatures

--- grondbalans-werken.php
+++ grondbalans-werken.php
@@ -20,12 +20,42 @@ get_header();?>
                 <h1>WERKEN IN EEN GEDREVEN TEAM</h1>
                 <h2>Doen, leren & groeien!</h2>
                 <p>
-                Werken bij Grondbalans is iedere dag weer anders. Initiatief nemen, nieuwe ideeën opperen: neem het heft in eigen handen! Zo haal je telkens het beste uit jezelf. Dat past bij ons als platte organisatie met een ondernemend karakter. Jouw ontwikkeling vinden we erg belangrijk. Ook aan het teamgevoel hechten wij veel waarde. Grondbalans bestaat uit een team met 25 enthousiaste mensen, uit alle hoeken van het land. Iedereen heeft eigen verantwoordelijkheden, maar kan altijd terugvallen op het team. Een team waarbij je lekker jezelf kunt zijn.
+                    Werken bij Grondbalans is iedere dag weer anders. Initiatief nemen, nieuwe ideeën opperen: neem het heft in eigen handen! Zo haal je telkens het beste uit jezelf. Dat past bij ons als platte organisatie met een ondernemend karakter. Jouw ontwikkeling vinden we erg belangrijk. Ook aan het teamgevoel hechten wij veel waarde. Grondbalans bestaat uit een team met 25 enthousiaste mensen, uit alle hoeken van het land. Iedereen heeft eigen verantwoordelijkheden, maar kan altijd terugvallen op het team. Een team waarbij je lekker jezelf kunt zijn.
                 </p>
+                <iframe id="werkenbij" width="1000" height="500"  src="https://www.youtube.com/embed/cg-tgT16EUg" frameborder="0" allowfullscreen></iframe>
+                <h3>BEN JIJ TOE AAN EEN VOLGENDE STAP?</h3>
+                <p style="margin-bottom: 3rem;">
+                    Wil jij meegroeien met ons? Denk jij dat je bij ons past en dat je een waardevolle bijdrage kunt leveren aan onze organisatie en dienstverlening? Is de GWW-branche helemaal jouw wereldje en word je blij van ambitieuze collega's en tevreden opdrachtgevers? Check dan onze vacatures!
+                    <br/>
+                    Staat jouw ideale functie er niet tussen, stuur ons dan een open sollicitatie. Wij vinden het altijd interessant om te horen wat jouw ambities zijn.
+                </p>
+
+                <iframe id="werkenbij" width="1000" height="500"  src="https://www.youtube.com/embed/6wYppiFuR7w" frameborder="0" allowfullscreen></iframe>
+                <div class="content__werken--left--cta">
+                    <h3>NEEM DIRECT CONTACT OP</h3>
+                    <p>
+                    Meer weten over werken bij Grondbalans of interesse in een vacature? Wil je een open sollicitatie sturen?
+                    Neem contact op met Wendy Stolwijk via <a href="mailto:vacature@grondbalans.nl">vacature@grondbalans.nl</a> of bel 06 45222774.
+                    </p>
+                </div>
             </div>
 
             <div class="content__werken--right">
-                
+                <div class="content__werken--vacatures">
+                    <h3>ONZE VACATURES</h3>
+                    <p>Sorry, we hebben op dit moment geen vacatures.</p>
+                </div>
+
+                <div class="content__werken--quote">
+                    <p>“</p>
+                    <p>Grondbalans is een club mensen die er echt voor wil gaan! Het harde werken gaat hand in hand met plezier maken. Onze mensen krijgen alle ruimte en mogelijkheden om zich te ontwikkelen.
+                    Eigen initiatief is hierbij wel belangrijk, waarbij je altijd kunt terugvallen op het team.</p>
+                    <img src="https://mlmotgafwbhj.i.optimole.com/u0PFPh0-8wKPrU38/w:120/h:132/q:75/rt:fill/g:ce/https://grondbalans.nl/wp-content/uploads/2020/01/09-Dirkjan.jpg" width="150px" alt="Foto"/>
+                    <div class="content__werken--quote-bio">
+                        <p><strong>Dirkjan Reijersen van Buuren</strong></p>
+                        <p>Directeur Grondbalans</p>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
--- template-parts/portfolio-block.php
+++ template-parts/portfolio-block.php
@@ -31,7 +31,7 @@ $link = substr(strstr($ref, '='), strlen('='));
 
         <div class="portfolio__block--right">
             <?php if ($args['video']) :?>
-                <iframe id="myIframe" width="560" height="315"  src="https://www.youtube.com/embed/<?php echo $link;?>" frameborder="0" allowfullscreen></iframe>
+                <iframe id="video" width="560" height="315"  src="https://www.youtube.com/embed/<?php echo $link;?>" frameborder="0" allowfullscreen></iframe>
                 <!-- <iframe id="video-6-2_youtube_iframe" frameborder="0" allowfullscreen="1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" title="YouTube video player" src="https://www.youtube.com/embed/1_AJ9QLsm10?controls=0&amp;rel=0&amp;disablekb=1&amp;showinfo=0&amp;modestbranding=0&amp;html5=1&amp;iv_load_policy=3&amp;autoplay=0&amp;end=0&amp;loop=0&amp;playsinline=0&amp;start=0&amp;nocookie=false&amp;enablejsapi=1&amp;origin=https%3A%2F%2Fgrondbalans.nl&amp;widgetid=1" width="575" height="323.4375"></iframe> -->
             <?php else :?>
                 <img src="<?php echo $args['right']['url'];?>" alt="Project afbeelding" width="720px" />
