3f99a63439e695b4e8171fad1667a5e30e4be7e1
Leroy Davidson Descelles <leroy.davidsondescelles@zoolt.com>
2021-01-19 13:18:37 +0100

    vacatures

---
+++ grondbalans-werken.php
@@ -0,0 +1,37 @@
+<?php
+//Template name: Werken bij
+
+get_header();?>
+<?php
+    $args = array(
+        'background' => "https://mlmotgafwbhj.i.optimole.com/u0PFPh0-GAfKxHUN/w:auto/h:auto/q:75/https://grondbalans.nl/wp-content/uploads/2020/06/werken-bij-grondbalans-1920-02.jpg",
+        'heading' => "WERKEN BIJ GRONDBALANS",
+        'content' => "NEEM HET HEFT IN EIGEN HANDEN",
+        'button' => "",
+    );
+    get_template_part( 'template-parts/banner', 'general', $args );
+?>
+
+
+<div class="content">
+    <div class="content__container">
+        <div class="content__werken--container">
+            <div class="content__werken--left">
+                <h1>WERKEN IN EEN GEDREVEN TEAM</h1>
+                <h2>Doen, leren & groeien!</h2>
+                <p>
+                Werken bij Grondbalans is iedere dag weer anders. Initiatief nemen, nieuwe ideeën opperen: neem het heft in eigen handen! Zo haal je telkens het beste uit jezelf. Dat past bij ons als platte organisatie met een ondernemend karakter. Jouw ontwikkeling vinden we erg belangrijk. Ook aan het teamgevoel hechten wij veel waarde. Grondbalans bestaat uit een team met 25 enthousiaste mensen, uit alle hoeken van het land. Iedereen heeft eigen verantwoordelijkheden, maar kan altijd terugvallen op het team. Een team waarbij je lekker jezelf kunt zijn.
+                </p>
+            </div>
+
+            <div class="content__werken--right">
+                
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+<?php get_footer();?>
