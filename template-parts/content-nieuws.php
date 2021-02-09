f0e68be4c47c09ef7cd8fdd553e6b82e116cc83a
Nishkea <leroy.davidson@live.nl>
2021-02-09 11:39:35 +0100

    ie

--- template-parts/content-nieuws.php
+++ template-parts/content-nieuws.php
@@ -6,19 +6,19 @@
 
 <div class="nieuws__page--content">
 
-<?php
- $thumb = wp_get_attachment_image_src(get_post_thumbnail_id(), 'thumbnail_name');
- echo '<img src="'.$thumb[0].'" alt="Nieuws afbeelding" />';
-?>
-<?php echo the_title();?>
-<?php
-$query = get_post(get_the_ID()); 
-$content = apply_filters('the_content', $query->post_content);
-echo $content;
-;?>
+    <?php
+    $thumb = wp_get_attachment_image_src(get_post_thumbnail_id(), 'thumbnail_name');
+    echo '<img src="'.$thumb[0].'" alt="Nieuws afbeelding" />';
+    ?>
+    <?php echo the_title();?>
+    <?php
+    $query = get_post(get_the_ID()); 
+    $content = apply_filters('the_content', $query->post_content);
+    echo $content;
+    ;?>
 </div>
 <div class="nieuws__page--sidebar">
-<?php get_template_part('archive/sidebar', 'nieuws');?>
+    <?php get_template_part('archive/sidebar', 'nieuws');?>
 </div>
 </div>
 
