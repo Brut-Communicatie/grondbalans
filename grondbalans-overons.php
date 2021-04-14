b3615d5c70fadeabd9f5c32f84b8b434f4ba7b79
TMB <tmb@TMB-1.local>
2021-04-14 10:51:35 +0200

    content

--- grondbalans-overons.php
+++ grondbalans-overons.php
@@ -4,16 +4,6 @@ get_header();?>
 <?php while ( have_posts() ) : the_post(); ?>
 
 <?php the_content();?>
-<div class="content">
-    <div class="content__container">
-        <div class="content__intro">
-            <h1>Wij zijn <br/> grondbalans</h1>
-            <h2>Samen mooie projecten maken!</h2>
-            <p>Wij zijn echte ondernemers: aanpakkers die altijd voor het beste resultaat gaan. We houden ons aan ons woord en beloven geen dingen die we niet kunnen waarmaken. Afspraak is afspraak. Dé basis voor een goede samenwerking. Want, ‘samen mooie projecten maken’, daar staan wij voor! Ons landelijke netwerk, onze uitgebreide kennis en onze onafhankelijke positie maken Grondbalans een unieke organisatie in deze sector. Daar zijn we best trots op, dat snap je wel.</p>
-            <div class="content__line"></div>
-        </div>
-    </div>
-</div>
 
 <div class="expertise">
     <div class="expertise__container">
