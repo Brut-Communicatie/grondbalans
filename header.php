11831089cd2c02c5d217428a143da2326418a5bd
Leroy Davidson Descelles <leroy@Leroys-MacBook-Pro-3.local>
2021-05-25 13:55:02 +0200

    aos

--- header.php
+++ header.php
@@ -18,7 +18,7 @@
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 	<link rel="preconnect" href="https://fonts.gstatic.com">
 	<link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
-	<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
 	<?php wp_head(); ?>
 	<style>
 body {
